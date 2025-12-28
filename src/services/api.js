86dac42014cd863d7038fe00ec7b801738806bf7
Fehed Shushan <fehed165@gmail.com>
2025-12-28 23:31:23 +0100

    Registerflöde implementerat med CSRF-hantering (fungerar i produktion

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: " https://chatify-api.up.railway.app",
-    withCredentials: true
+    baseURL: "/api",
+    withCredentials: true,
 });
 
 // hämtar CSRF token
