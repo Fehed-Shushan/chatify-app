a5996d857725d05a7bc149455d8c9860af22e5af
Fehed Shushan <fehed165@gmail.com>
2025-12-28 21:17:08 +0100

    Gjorde ändringar register form med kontroll inmatning

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -7,4 +7,55 @@ function Register () {
     const [password, setPassword] = useState ("");
     const [ avatar, setAvatar] = useState ("");
 
+function handleSubmit( e ) {
+    e.preventDefault();
+
+    console.log("Username:",username);
+    console.log("Email:",email) ;
+    console.log("Password:",password);
+    console.log("Avatar:",avatar);
 }
+
+return(
+    <div>
+        <h2>Register</h2>
+        <form onSubmit={handleSubmit}>
+            <div> 
+                <label>Username</label><br />
+                <input
+                type="text"
+                value={username}
+                onChange={( e ) => setUsername (e.target.value) }
+                />
+                
+    </div>
+
+    <div>
+        <label>Password</label><br />
+        <input
+        type="password"
+        value={password}
+        onChange={( e ) => setPassword(e.target.value) }
+        />
+        </div>
+
+        <div>
+            <label>Avatar URL</label>< br />
+            <input
+            type="text"
+            value={avatar}
+            onChange={( e ) => setAvatar(e.target.value)}
+            placeholder="https://i.pravatar.cc/200" // Enl uppgiften//
+            />
+
+   
+              </div>
+
+              <button type="submit">Register</button>
+
+
+        </form>
+    </div>
+) ;
+}
+export default Register; 
