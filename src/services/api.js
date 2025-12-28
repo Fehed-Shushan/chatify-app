120fd0b3332e1c491260e9a803e89bfa2acbbcde
Fehed Shushan <fehed165@gmail.com>
2025-12-28 21:41:11 +0100

    la till API service med axios och CSRF

---
+++ src/services/api.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: " https://chatify-api.up.railway.app",
+    withCredentials: true
+});
+
+// hämtar CSRF token
+export async function getCsrfToken() {
+    const response = await api.patch ("/csrf");
+    return response.data.csrfToken;
+}
+
+// Register av ny användare
+ export async function registerUser(userData) {
+    const csrfToken = await getCsrfToken();
+
+    const response = await api.post(
+        "/auth/register",
+        userData,
+        {
+            headers : {}
+        }
+    )
+ }
